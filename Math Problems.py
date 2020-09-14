dfb185c2b7c2ced60074cdb63616f49c2190c82f
Csaintilienc1 <39477225+Csaintilienc1@users.noreply.github.com>
2020-09-14 13:26:45 -0400

    Add files via upload

---
+++ Math Problems.py
@@ -0,0 +1,76 @@
+#Math Problems
+#Math Problems.py
+#Claudel Saintilien
+#November 15, 2014
+#November 20, 2014
+#Demonstrate use of input and print statements.
+
+
+
+
+
+print("1.Suppose Bruce Lee was training with 3 ninjas,")
+print("but he's defeated 2 of them. How many ninjas left does he have")
+print("to train with?")
+input("Answer")
+print("3-2=1")
+
+
+
+
+print("2.If Dj Ridd plays 5 songs per party,")
+print("what is the total amount songs he has played going to 2 parties?")
+input("Answer")
+print("5*2=10")
+
+
+
+print("3.Sonic the Hedgehog has collected 100 rings,"
+      "but eventually splits half to Tails.")
+print("How many rings does Sonic have left?")
+input("Answer")
+
+print("100/2=50")
+
+
+
+
+print("4.Spider-Man plans to web swing over 10 buildings,"
+      "but 6 of them are under construction.")
+print("How many building left does Spider-Man have to web swing through?")
+input("Answer")
+print("10-6=4")
+
+
+
+print("5. On the official launch Day of Super Smash Bros for Wii U,")
+print("200 customers bombarded the once controlled store of"
+      "Walmart to purchase a copy. Time has passed and half of,")
+print("the customers have left.")
+print("How many customers at Walmart are left?")
+input("Answer")
+print("200/2=100")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+      
+
